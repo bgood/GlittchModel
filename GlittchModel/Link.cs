4153a5edee601c15ab3603b7932939f520817291
bgood <good.benjamin@gmail.com>
2016-03-28 19:57:25 -0600

    Initial commit, basic body outline, coxa and drawing code.

---
+++ GlittchModel/Link.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlittchModel
+{
+    public class Link
+    {
+        public Link(Point center, int length)
+        {
+            CenterOfRotation = center;
+            Length = length;
+        }
+
+        public Point CenterOfRotation { get; set; }
+        public Point EndPoint
+        {
+            get
+            {
+                return new Point(CenterOfRotation.X + Length, CenterOfRotation.Y);
+            }
+        }
+        public int Length;
+    }
+}
---
+++ GlittchModel/ModelView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlittchModel
+{
+    public partial class ModelView : Form
+    {
+        private const int scale = 3;
+        private const int x_offset = 250;
+        private const int y_offset = 100;
+
+        private List<Link> Coxas = new List<Link>();
+
+        public ModelView()
+        {
+            InitializeComponent();
+
+            for(int i = 0; i < 3; i++)
+            {
+                int y_spacing = 95 * scale;
+                int x_spacing = 130 * scale;
+
+                Coxas.Add(new Link(new Point(x_offset + 20 * scale, y_offset + 20 * scale + i * y_spacing), -40 * scale));
+                Coxas.Add(new Link(new Point(x_offset + 20 * scale + x_spacing, y_offset + 20 * scale + i * y_spacing), 40 * scale));
+            }
+        }
+
+        private void ModelView_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            DrawBody(e);
+        }
+
+        private void DrawBody(System.Windows.Forms.PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 2);
+            Rectangle bodyOutline = new Rectangle(250, 100, 170 * scale, 230 * scale);
+
+            e.Graphics.DrawRectangle(blackPen, bodyOutline);
+
+            foreach (Link l in Coxas)
+                DrawCoxa(l, e);
+
+            blackPen.Dispose();
+        }
+
+        private void DrawCoxa(Link l, System.Windows.Forms.PaintEventArgs e)
+        {
+            Pen greenPen = new Pen(Color.Green, 2);
+            e.Graphics.DrawLine(greenPen, l.CenterOfRotation, l.EndPoint);
+
+            greenPen.Dispose();
+        }
+    }
+}
